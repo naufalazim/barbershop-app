0a201562529386dce40453471adf69df03131680
Naufal Azim <naufal.azim@outlook.com>
2022-01-08 19:41:12 +0800

    initial commit 1.0v

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleheader.css">
+    <title>Header</title>
+</head>
+<body>
+
+  <nav>
+    <div class="logo">
+      <h2>Tuah Barbershop</h2>
+    </div>
+    
+    <ul class="nav-links">
+      <li><a href="#">Customer</a></li>
+      <li><a href="#">Owner</a></li>
+      <li><a href="#">Admin</a></li>
+    </ul>
+    
+  </nav>
+    
+</body>
+</html>
