1ffdeacddd295e4c76e4be1f9f9bd8b84e7b073c
Naufal Azim <naufal.azim@outlook.com>
2022-01-10 00:04:28 +0800

    1.1v
    Develop:
    
    - Admin login
    - Header some change

---
+++ adminlogin.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="adminlogin.css">
+    
+    <title>Admin Login</title>
+</head>
+<body>
+
+    <!-- PART: Header -->
+    <?php include 'head.php'  ?>
+
+    <!-- PART: Login  -->
+    <div class="container">
+
+        <div class="box">
+
+            <!-- <div class="title"> -->
+                <h4>Admin Login</h4>
+            <!-- </div> -->
+
+            <form class="form" action="" method="POST">
+                <label><p>Username:</p> </label>
+                <input type="text" class="" placeholder="Admin Username"  value=""> <br>
+
+                
+                <label><p>Password:</p>  </label>
+                <input type="password" class="" placeholder="Admin Password" value="">
+
+                <div class="submit-btn">
+                <br><input class="btn" type="submit" value="Login">
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
--- head.php
+++ head.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="head.css">
+    <!-- <link rel="stylesheet" href="head.css"> -->
+    <style>
+     <?php include "head.css" ?> 
+    </style>
+
     <title>Header</title>
 </head>
 <body>
@@ -18,7 +22,7 @@
     <ul class="nav-links">
         <li><a href="#">Customer</a></li>
         <li><a href="#">Barber</a></li>
-        <li><a href="#">Admin</a></li>
+        <li><a href="adminlogin.php">Admin</a></li>
     </ul>
 
     </nav>
