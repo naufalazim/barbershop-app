27cea81f9300783242b5385ab55f587e5d41cfb5
Naufal Azim <naufal.azim@outlook.com>
2022-01-10 16:16:27 +0800

    Edit JS

--- adminlogin.php
+++ adminlogin.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="adminlogin.css">
-    <script type="text/javascript" src="test.js"></script>
+    <!-- <script type="text/javascript" src="test.js"></script> -->
     
     <title>Admin Login</title>
 </head>
