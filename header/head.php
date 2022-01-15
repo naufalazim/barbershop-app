5c245f90548d753b5b15696802047076301c8fb6
Naufal Azim <naufal.azim@outlook.com>
2022-01-15 18:20:28 +0800

    Header Std

--- header/head.php
+++ header/head.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="head.css"> -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <style>
      <?php include "head.css" ?> 
     </style>
@@ -12,20 +13,24 @@
     <title>Header</title>
 </head>
 <body>
-
+    <header>
     <nav>
-
     <div class="logo">
         <h2><a href="/barbershop-app//index.php">Tuah.</a></h2>
     </div>
 
-    <ul class="nav-links">
-        <li><a href="#">Customer</a></li>
-        <li><a href="#">Staff</a></li>
-        <li><a href="/barbershop-app/admin/adminlogin.php">Admin</a></li>
-    </ul>
+    <!-- <ion-icon name="menu-outline" class="toggle-menu"></ion-icon> -->
 
+    <ul class="nav-links">
+        <li><a href="#" class="nav-items">Customer</a></li>
+        <li><a href="#" class="nav-items">Staff</a></li>
+        <li><a href="/barbershop-app/admin/adminlogin.php" class="nav-items">Admin</a></li>
+    </ul>
+    <!-- <ion-icon name="close-outline" class="toggle-close"></ion-icon> -->
     </nav>
+    </header>
     
 </body>
 </html>
+
+
